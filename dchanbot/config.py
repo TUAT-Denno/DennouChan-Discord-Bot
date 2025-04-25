164da8ca1c9076a62001a63641fd65ff5da1c2a4
baobao-ao32 <aomusi.32trpro@gmail.com>
2025-04-25 15:48:56 +0900

    config.pyを追加

---
+++ dchanbot/config.py
@@ -0,0 +1,50 @@
+import logging
+import json
+from pathlib import Path
+
+from bot import DChanBot
+
+logger = logging.getLogger("dchanbot.config")
+
+class Config:
+    def __init__(self, confdir : Path, name : str, bot : DChanBot):
+        if confdir.exists() is False:
+            raise FileNotFoundError(f"Path is not found: {confdir}")
+        if confdir.is_dir() is False:
+            raise NotADirectoryError(f"Not a directory: {confdir}")
+
+        self._confdir = confdir
+        self._confpath = confdir / f"{name}.json"
+
+        self._bot = bot
+        self._confcache = {}    # 設定データのメモリキャッシュ
+
+    # 設定をファイルから読み込む
+    def load(self):
+        if self._confpath.exists():
+            with self._confpath.open(mode="r") as f:
+                self._confcache = json.load(f)
+
+    # 設定をファイルに保存
+    def save(self):
+        with self._confpath.open(mode = 'w') as f:
+            json.dump(self._confcachel, f, indent = 4)    
+
+    # 指定された設定データの取得
+    def get(self, *keys, default=None):
+        data = self._confcache
+        try:
+            for key in keys:
+                data = data[key]
+            return data
+        except:
+            return default  # 見つからなければ規定値を返す
+
+    # 設定データの変更
+    def set(self, value, *keys):
+        data = self._confcache
+        for key in keys[:-1]:
+            if key not in data or not isinstance(data[key], dict):
+                data[key] = {}
+            data = data[key]
+        data[keys[-1]] = value
