b2b61c122921fa80d1bf23e798d4a85aa3a12f5d
baobao-ao32 <aomusi.32trpro@gmail.com>
2025-06-19 15:48:04 +0900

    dict_to_jsonとdict_from_jsonの追加

--- dchanbot/core/json_bound_model.py
+++ dchanbot/core/json_bound_model.py
@@ -1,10 +1,29 @@
 import json
 from pathlib import Path
-from typing import Type, TypeVar, Generic, Any
+from typing import Type, TypeVar, Generic, Any, Dict
 
 from pydantic import BaseModel
 
 _T = TypeVar('T', bound = BaseModel)
+
+
+def dict_to_json(dic : Dict[str, _T], jsonfilename : Path):
+    jsonpath = jsonfilename.resolve()
+    jsonpath.parent.mkdir(parents = True, exist_ok = True)
+
+    serializable = {k: v.model_dump() for k, v in dic.items()}
+    with jsonpath.open("w", encoding = "utf-8") as file:
+        json.dump(serializable, file, indent = 4)
+
+def dict_from_json(jsonfilename : Path, model_class : Type[_T]) -> Dict[str, _T]:
+    jsonpath = jsonfilename.resolve()
+    if not jsonpath.exists():
+        return {}
+
+    with jsonpath.open("r", encoding = "utf-8") as file:
+        raw_data = json.load(file)
+    return {k: model_class(**v) for k, v in raw_data.items()}
+
 
 class JSONBoundModel(Generic[_T]):
     def __init__(self, jsonfilename : Path, schema : Type[_T]):
